218c7d840f92073561ef51878561ea7d8ab18461
Musibau Opeyemi <binson220@gmal.com>
2022-04-07 14:52:16 -0700

    New commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*  This program prints output using the puts function */
+int main(void)
+{
+	char str = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
