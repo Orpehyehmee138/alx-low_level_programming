dd8d84575ed4869af2025ee625db5e73d52d9d9c
Musibau Opeyemi <binson220@gmal.com>
2022-04-08 18:32:50 -0700

    Night

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	char my_array[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z', '\n'};
+	int i;
+
+	for (i = 0; i <= 26; i++)
+	{
+		if (my_array[i] == 'e' || my_array[i] == 'q')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(my_array[i]);
+		}
+	}
+	return (0);
+}
