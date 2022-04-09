3bda813c9b4f1645146435e8a085dea4dfe9f90a
Musibau Opeyemi <binson220@gmal.com>
2022-04-09 15:18:07 -0700

    here we go again

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,12 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		putchar((char)i);
-		putchar(',');
-		putchar(' ');
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	return (0);
 }
