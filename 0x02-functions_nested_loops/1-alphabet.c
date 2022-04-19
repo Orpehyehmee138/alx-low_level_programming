86e2cd8917ee0e800d122ab80a070b4af75c5b68
Musibau Opeyemi <binson220@gmal.com>
2022-04-18 18:13:51 -0700

    New commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ * @print_alphabet: It's a void function
+ * return: Always(0) Success
+ */
+void print_alphabet(void);
+int main (void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	int ch;
+	for(ch = 97; ch <= 122; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
