1b732be4ef5035e4fb945887bc0ecccbef0f685e
Musibau Opeyemi <binson220@gmal.com>
2022-04-27 18:32:34 -0700

    Nepagit add .!

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: int type number
+ * Return : Absolute value of @n 1
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
