071bd67e0423f86ea2974a283a22572e335523fd
Musibau Opeyemi <binson220@gmal.com>
2022-04-27 18:41:07 -0700

    Nepagit add .!

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit of a number
+ * @n: The number to be treated
+ * Return: Value of the last number
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if (last < 10)
+		last = last * -1;
+	_putchar(last + '0');
+	return (last);
+}
