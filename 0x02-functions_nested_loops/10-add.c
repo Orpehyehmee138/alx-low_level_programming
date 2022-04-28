be2c3e068de3cf46a214929fd79e3ba84c7f2970
Musibau Opeyemi <binson220@gmal.com>
2022-04-27 19:36:19 -0700

    Hurry up

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two numbers
+ * @n1: The first integer
+ * @n2: The second integer
+ * Return: Returns an integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
