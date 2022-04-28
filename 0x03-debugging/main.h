2791ed2865a4b94385a96b6af00df38a159688a3
Musibau Opeyemi <binson220@gmal.com>
2022-04-27 21:28:06 -0700

    Hurry up

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,5 +3,8 @@
 
 #include <stdio.h>
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int conver_day(int month, int day);
+void print_remaining_days(int monh, int day, int year,);
 
 #endif /*MAIN_H*/
